8a3d24ac147668f1e3da2d184a64c77a6b596c53
Tomasz Przyborowski <t.s.przyborowski@gmail.com>
2020-02-17 21:15:29 +0100

    fix timer, add time records

--- timer.js
+++ timer.js
@@ -1,6 +1,21 @@
+// ACTUAL DATE
+const h1 = document.querySelector(`h1[data-time="day"]`);
+const h2 = document.querySelector(`h2[data-time="month"]`);
+
+const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+let today = new Date();
+let dayNumber = today.getDay();
+let monthNumber = today.getMonth();
+let year = today.getFullYear();
+
+h2.textContent = `${dayList[dayNumber]}, ${monthList[monthNumber]}, ${year}`;
+
 const initBtn = document.querySelector('.main');
 const resetBtn = document.querySelector('.reset');
 const timerOutput = document.querySelector('.time');
+const ul = document.querySelector(".times");
 
 let time = 0;
 let active = false;
@@ -18,9 +33,12 @@ const initTimer = () => {
     }
 };
 
+let newRecord = "";
+let i = 1;
 const startTimer = () => {
     time++;
-    timerOutput.textContent = (time / 100).toFixed(2);
+    timerOutput.textContent = `${(time / 1000).toFixed(2)}`;
+    newRecord = timerOutput.textContent;
 };
 
 const resetTimer = () => {
@@ -29,6 +47,14 @@ const resetTimer = () => {
     initBtn.textContent = 'START';
     timerOutput.textContent = '---';
     clearInterval(intervalId);
+
+    if (!newRecord) return;
+
+    const newLi = document.createElement("li");
+    newLi.classList = "record__item";
+    ul.insertBefore(newLi, ul.firstChild).innerHTML = `<span class="check-text"> Measurement ${i} is equal to <strong>${newRecord}</strong>`;
+    newRecord = "";
+    i++;
 };
 
 initBtn.addEventListener('click', initTimer);
