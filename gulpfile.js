e20c81586f0d57abc94b5704fe15856e61359105
Tomasz Przyborowski <t.s.przyborowski@gmail.com>
2020-05-08 10:53:18 +0200

    add scss to css compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass");
 const browserSync = require("browser-sync").create();
+
+function style () {
+    return (gulp.src("./scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream())
+    );
+};
 
 function watch() {
     browserSync.init({
@@ -7,9 +16,10 @@ function watch() {
             baseDir: './'
         }
     })
-    gulp.watch('./css/**/*.css').on('change', browserSync.reload);
+    gulp.watch('./scss/**/*.scss', style);
     gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 };
 
+exports.style = style;
 exports.watch = watch;
