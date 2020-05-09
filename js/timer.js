5017fbbe44e8dfd70f28e6fffeb154e1fc062736
Tomasz Przyborowski <t.s.przyborowski@gmail.com>
2020-05-09 12:12:58 +0200

    fix timer functions

--- js/timer.js
+++ js/timer.js
@@ -1,17 +1,17 @@
-// ACTUAL DATE
+// Current date
 const h1 = document.querySelector(`h1[data-time="day"]`);
 const h2 = document.querySelector(`h2[data-time="month"]`);
 
-const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+function renderDate() {
+  let today = new Date();
+  let dayNumber = today.getDate();
+  let month = today.toLocaleString('locale', {month: 'short'});
+  let year = today.getFullYear();
+  let day = today.toLocaleDateString('locale', {weekday: 'long'});
 
-let today = new Date();
-let dayNumber = today.getDay();
-let monthNumber = today.getMonth();
-let year = today.getFullYear();
-let day = today.getDate();
-
-h2.textContent = `${dayList[dayNumber]}, ${day} ${monthList[monthNumber]}, ${year}`;
+  h2.textContent = `${day}, ${dayNumber} ${month}, ${year}`;
+}
+renderDate();
 
 const initBtn = document.querySelector('.buttons__main');
 const resetBtn = document.querySelector('.buttons__reset');
@@ -26,81 +26,80 @@ let active = false;
 let intervalId;
 
 const initTimer = () => {
-    if (!active) {
-        active = !active;
-        initBtn.textContent = 'PAUSE';
-        intervalId = setInterval(startTimer, 1000);
-    } else {
-        active = !active;
-        initBtn.textContent = 'START';
-        clearInterval(intervalId);
-    };
+  if (!active) {
+    active = !active;
+    initBtn.textContent = 'PAUSE';
+    intervalId = setInterval(startTimer, 1000);
+  } else {
+    active = !active;
+    initBtn.textContent = 'START';
+    clearInterval(intervalId);
+  };
 };
 
 let newRecord = "";
 
 const startTimer = () => {
-    var time = now;
-    var hours = Math.floor(time / 3600);
-    time -= hours * 3600;
-    var mins = Math.floor(time / 60);
-    time -= mins * 60;
-    var secs = time;
+  var time = now;
+  var hours = Math.floor(time / 3600);
+  time -= hours * 3600;
+  var mins = Math.floor(time / 60);
+  time -= mins * 60;
+  var secs = time;
 
-    // Update the display timer
-    if (hours < 10) {
-        hours = +hours;
-    }
-    if (mins < 10) {
-        mins = "0" + mins;
-    }
-    if (secs < 10) {
-        secs = "0" + secs;
-    }
-    timerOutput.innerHTML = hours + "." + mins + "." + secs;
+  // Update the display timer
+  if (hours < 10) {
+    hours = +hours;
+  }
+  if (mins < 10) {
+    mins = "0" + mins;
+  }
+  if (secs < 10) {
+    secs = "0" + secs;
+  }
+  timerOutput.innerHTML = hours + "." + mins + "." + secs;
 
-    newRecord = timerOutput.textContent;
-    localStorage.setItem("time", JSON.stringify(now));
-    now++;
+  newRecord = timerOutput.textContent;
+  localStorage.setItem("time", JSON.stringify(now));
+  now++;
 };
 
 function resetTimer(e) {
+  now = 0;
+  active = false;
+  initBtn.textContent = 'START';
+  timerOutput.textContent = '0.00.00';
+  clearInterval(intervalId);
+  localStorage.removeItem("time")
+  localStorage.setItem("i", JSON.stringify(i));
 
-    now = 0;
-    active = false;
-    initBtn.textContent = 'START';
-    timerOutput.textContent = '0.00.00';
-    clearInterval(intervalId);
-    localStorage.removeItem("time")
-    localStorage.setItem("i", JSON.stringify(i));
-
-    if (!newRecord) return;
-    record = document.createElement('div');
-    record.innerHTML = `<li> Measurement ${i} is equal to <strong>${newRecord}</strong></li>`;
-    ul.appendChild(record);
-    newRecord = "";
-    i++;
-    records.push(record.innerHTML);
-    updateRecords(records, ul);
+  if (!newRecord) return;
+  record = document.createElement('div');
+  record.innerHTML = `<li> Measurement ${i} is equal to <strong>${newRecord}</strong></li>`;
+  ul.appendChild(record);
+  newRecord = "";
+  i++;
+  records.push(record.innerHTML);
+  updateRecords(records, ul);
 };
 
 function updateRecords(records = [], ul) {
-    ul.innerHTML = records.map((record) => {
-        console.log(record);
-        return record;
-    }).join('');
-    localStorage.setItem("records", JSON.stringify(records));
+  ul.innerHTML = records.map((record) => {
+    console.log(record);
+    return record;
+  }).join('');
+  localStorage.setItem("records", JSON.stringify(records));
 };
 
 initBtn.addEventListener('click', initTimer);
 resetBtn.addEventListener('click', resetTimer);
 
 function archiveRecords() {
-    records = [];
-    i = 1;
-    localStorage.removeItem("records");
-    localStorage.removeItem("i");
-    updateRecords(records, ul);
+  records = [];
+  i = 1;
+  localStorage.removeItem("records");
+  localStorage.removeItem("i");
+  updateRecords(records, ul);
 };
 
 archiveIcon.addEventListener("click", archiveRecords);
